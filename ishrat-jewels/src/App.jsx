38fc0d7b57dc65dc34848e424a47149f78278e8d
Samia Siddique <samia.siddique34@example.com>
2026-05-08 18:14:38 +0500

    fix app render

--- ishrat-jewels/src/App.jsx
+++ ishrat-jewels/src/App.jsx
@@ -1,4 +1,24 @@
 import React from "react";
-export default function App() {
-  return <h1>It works 🚀</h1>;
-}
+import Hero from "./components/Hero/Hero";
+import Footer from "./components/Footer/Footer";
+import Products from "./components/Product/Product";
+
+const App = () => {
+  return (
+    <div>
+      {" "}
+      <section id="home">
+        <Hero />
+      </section>
+      <section id="shop">
+        <Products />
+      </section>
+      <section id="contact">
+        <Footer />
+      </section>
+    </div>
+  );
+};
+
+export default App;
+
